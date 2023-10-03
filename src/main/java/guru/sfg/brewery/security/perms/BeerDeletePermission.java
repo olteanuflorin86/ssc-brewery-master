02e1ef62c7513d6894a26535b4804ce3c508cad9
Florin Olteanu <olteanuflorin86@gmail.com>
2023-10-03 19:29:16 +0300

    adding custom auth annotation

---
+++ src/main/java/guru/sfg/brewery/security/perms/BeerDeletePermission.java
@@ -0,0 +1,13 @@
+package guru.sfg.brewery.security.perms;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority('beer.delete')")
+public @interface BeerDeletePermission {
+
+}
+
