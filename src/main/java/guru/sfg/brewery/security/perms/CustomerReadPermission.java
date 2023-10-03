21a3f631bde7fc2d97cb97b2df74e55a4a541a9e
Florin Olteanu <olteanuflorin86@gmail.com>
2023-10-03 19:45:44 +0300

    completing assignment to add annotations

---
+++ src/main/java/guru/sfg/brewery/security/perms/CustomerReadPermission.java
@@ -0,0 +1,12 @@
+package guru.sfg.brewery.security.perms;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority('customer.read')")
+public @interface CustomerReadPermission {
+
+}
