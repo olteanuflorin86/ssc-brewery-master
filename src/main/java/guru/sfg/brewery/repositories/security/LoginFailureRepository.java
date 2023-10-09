25d5fa0e8af529e2fcc3cbf8986d6a00cfc9980f
Florin Olteanu <olteanuflorin86@gmail.com>
2023-10-09 18:43:10 +0300

    adding lock user account

--- src/main/java/guru/sfg/brewery/repositories/security/LoginFailureRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/security/LoginFailureRepository.java
@@ -1,9 +1,14 @@
 package guru.sfg.brewery.repositories.security;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import guru.sfg.brewery.domain.security.LoginFailure;
+import guru.sfg.brewery.domain.security.User;
 
 public interface LoginFailureRepository extends JpaRepository<LoginFailure, Integer> {
 
+	List<LoginFailure> findAllByUserAndCreatedDateIsAfter(User user, Timestamp timestamp); 
 }
--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
@@ -1,5 +1,9 @@
 package guru.sfg.brewery.security.listeners;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
@@ -7,6 +11,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 import guru.sfg.brewery.domain.security.LoginFailure;
+import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.LoginFailureRepository;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -49,7 +54,22 @@ public class AuthenticationFailureListener {
 			
 			LoginFailure loginFailure =  loginFailureRepository.save(builder.build());
 			log.debug("Failure Event: " + loginFailure.getId());
+			
+			if (loginFailure.getUser() != null) {
+                lockUserAccount(loginFailure.getUser());
+            }
 		}
 	}
+	
+	private void lockUserAccount(User user) {
+        List<LoginFailure> failures = loginFailureRepository.findAllByUserAndCreatedDateIsAfter(user,
+                Timestamp.valueOf(LocalDateTime.now().minusDays(1)));
+
+        if(failures.size() > 3){
+            log.debug("Locking User Account... ");
+            user.setAccountNonLocked(false);
+            userRepository.save(user);
+        }
+    }
 
 }
