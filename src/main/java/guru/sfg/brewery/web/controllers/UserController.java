516a51c22344d188c5214d7b0d4b01780bfdd8bd
Florin Olteanu <olteanuflorin86@gmail.com>
2023-10-10 10:14:35 +0300

    adding user 2fa registration controller

---
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -0,0 +1,38 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+	private final UserRepository userRepository; 
+	
+	@GetMapping("/register2fa")
+	public String register2fa(Model model) {
+		
+		model.addAttribute("googleurl", "todo");
+		
+		return "user/register2fa";
+	}
+	
+	@PostMapping
+	public String confirm2Fa(@RequestParam Integer verifyCode) {
+		
+		// todo - impl
+		
+		return "index";
+	}
+	
+}
