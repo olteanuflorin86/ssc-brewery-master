034b91b5bd67cce8147d356ca2cc765065b51738
Florin Olteanu <olteanuflorin86@gmail.com>
2023-11-15 13:00:16 +0200

    generating QR code

--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -1,5 +1,6 @@
 package guru.sfg.brewery.web.controllers;
 
+import org.springframework.security.core.context.SecurityContextHolder; 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+
+import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,15 +23,28 @@ import lombok.extern.slf4j.Slf4j;
 public class UserController {
 
 	private final UserRepository userRepository; 
+	private final GoogleAuthenticator googleAuthenticator;
 	
 	@GetMapping("/register2fa")
 	public String register2fa(Model model) {
 		
-		model.addAttribute("googleurl", "todo");
+		User user = getUser();
+		
+		String url = GoogleAuthenticatorQRGenerator.getOtpAuthURL("Florin", user.getUsername(), 
+				googleAuthenticator.createCredentials(user.getUsername()));
+		
+		log.debug("Google QR URL: " + url);
+		
+//		model.addAttribute("googleurl", "todo");
+		model.addAttribute("googleurl", url);
 		
 		return "user/register2fa";
 	}
 	
+	private User getUser() {
+		return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	}
+
 	@PostMapping
 	public String confirm2Fa(@RequestParam Integer verifyCode) {
 		
