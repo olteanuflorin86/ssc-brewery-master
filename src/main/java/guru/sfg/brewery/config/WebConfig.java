d0b784cf502bb8a01c2d16a38588ccec483c5cd3
Florin Olteanu <olteanuflorin86@gmail.com>
2023-11-25 05:58:30 +0200

    adding spring mvc config

---
+++ src/main/java/guru/sfg/brewery/config/WebConfig.java
@@ -0,0 +1,19 @@
+package guru.sfg.brewery.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// TODO Auto-generated method stub
+//		WebMvcConfigurer.super.addCorsMappings(registry);
+		
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
+		
+	}
+
+}
